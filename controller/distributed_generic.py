065c537f627e84feead7972e6d3b4dfe91e0c293
Elerson R. S. Santos <elerss@gmail.com>
2021-04-23 13:25:22 -0300

    updating the controller

--- controller/distributed_generic.py
+++ controller/distributed_generic.py
@@ -34,7 +34,7 @@ class SimpleSwitchApplication(eBPFCoreApplication):
         self.cols = 0
         self.phi = 0
         self.lasttime = 0
-        self.ni_data = []
+        self.ni_data = {}
         try:
             self.sock
         except:
@@ -101,14 +101,17 @@ class SimpleSwitchApplication(eBPFCoreApplication):
             self.rows = rows
             self.data_id[int(pkt.dpid)] = (dst0, dst1, dst2)
             
-            for ip_ in self.ni_data:
+            for ip_ in self.ni_data[int(pkt.dpid)]:
                 self.data[int(pkt.dpid)].append([ip_, self.data_id[int(pkt.dpid)]])
                 
-            self.ni_data = []
+            self.ni_data[int(pkt.dpid)] = []
             
         else:
             print(pkt.dpid, ip_, self.data_id[int(pkt.dpid)])
-            self.ni_data.append(ip_)
+            if int(pkt.dpid) not in self.ni_data:
+            	self.ni_data[int(pkt.dpid)] = []
+            	
+            self.ni_data[int(pkt.dpid)].append(ip_)
             self.lasttime = lasttime
             #self.data[int(pkt.dpid)].append([ip_, self.data_id[int(pkt.dpid)]])
         
